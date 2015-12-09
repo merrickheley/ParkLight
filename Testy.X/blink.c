ba498cf8d3f8a92142152e4f2ad07b3da70cab33
Jonathan Holland <jonathan.holland8@gmail.com>
2015-12-09 22:02:50 +1000

    Explicitly disabling analog and comparator functionality in order to free pin uage
    - Standard TRIS port usage is overwritten by any other functionality of the pin

--- Testy.X/blink.c
+++ Testy.X/blink.c
@@ -14,11 +14,23 @@ void init(void) {
     INTCON0bits.GIE = 1; //Enable INTs globally
     INTCON1bits.T0IE = 1; // Enable timer 0 interrupts
     INTCON1bits.RBIE = 1; // Enable port b interrupts
+    INTCON1bits.CWIE = 0; // Disable comparators
     
     // TRIG is RC7 - Output
-    TRISC = 0b01111111;
+    TRISC = 0b01101111;
     // ECHO is RB3 - Input - RB Interrupt trigger
     TRISB = 0b11111111;
+
+    ANSEL = 0;
+    ADCON0 = 0;
+    ADRES = 0;
+    CM1CON0 = 0;      
+    CM2CON0 = 0;
+    VRCON = 0;
+    OPACON = 0;
+    
+    OSCCAL = 0b01111110;
+    OPTION = 0b00001000;
 }
 
 
@@ -26,10 +38,18 @@ void main()
 {
     init();
     while(1) {
+        RC4 = 1;
+        PORTCbits.RC4 = 1;
         if(INTCON0bits.T0IF == 1) {
             RC7 = 1;
             unsigned char dummy  = 0;
             unsigned char dummyTwo = 1;
+            dummy  = 0;
+            dummyTwo = 1;
+            dummy  = 0;
+            dummyTwo = 1;
+            dummy  = 0;
+            dummyTwo = 1;
             RC7 = 0;
             
             unsigned char readValue = RB3;
