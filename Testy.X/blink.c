a148e385afc96a1f11878f02f13c6d754f20cc00
Jonathan Holland <jonathan.holland8@gmail.com>
2015-12-16 23:56:24 +1000

    Adding pragma config to main file, implementing interrupt

--- Testy.X/blink.c
+++ Testy.X/blink.c
@@ -5,56 +5,56 @@
  * Created on 1 December 2015, 8:52 PM
  */
 
+#pragma config FOSC = INTRC_IO  // Oscillator (INTRC with I/O function on OSC2/CLKOUT)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (Disabled)
+#pragma config CP = OFF         // Code Protection bit (Code protection off)
+#pragma config IOSCFS = 8MHz    // Internal Oscillator Frequency Select (8 MHz INTOSC Speed)
+#pragma config CPSW = OFF       // Code Protection bit - Flash Data Memory (Code protection off)
+#pragma config BOREN = OFF      //  (BOR Disabled)
+#pragma config DRTEN = OFF      //  (DRT Disabled)
+ 
+#include <xc.h>
+#include <htc.h>
+#define _XTAL_FREQ 8000000
 #include <stdio.h>
 #include <stdlib.h>
 #include <pic16f570.h>
-#include <htc.h>
+
+#define TRUE 1
+#define FALSE 0
 
 void init(void) {
     INTCON0bits.GIE = 1; //Enable INTs globally
     INTCON1bits.T0IE = 1; // Enable timer 0 interrupts
-    INTCON1bits.RBIE = 1; // Enable port b interrupts
-    INTCON1bits.CWIE = 0; // Disable comparators
     
     // TRIG is RC7 - Output
     TRISC = 0b00000000;
-    // ECHO is RB3 - Input - RB Interrupt trigger
-    TRISB = 0b11111111;
 
-    ANSEL = 0;
-    ADCON0 = 0;
-    ADRES = 0;
-    CM1CON0 = 0;      
-    CM2CON0 = 0;
-    VRCON = 0;
-    OPACON = 0;
+    TMR0 = 1;
     
+    // Set max oscillation speed
     OSCCAL = 0b01111110;
+    // Set PS 1:1
     OPTION = 0b00001000;
-    PORTC = 0b00000000;
 }
-
 
 void main()
 {
     init();
+    RC4 = 1;
+    
     while(1) {
-        //RC4 = 1;
-        PORTC = 0b11111111;
-        if(INTCON0bits.T0IF == 1) {
-            RC7 = 1;
-            unsigned char dummy  = 0;
-            unsigned char dummyTwo = 1;
-            dummy  = 0;
-            dummyTwo = 1;
-            dummy  = 0;
-            dummyTwo = 1;
-            dummy  = 0;
-            dummyTwo = 1;
-            RC7 = 0;
-            
-            unsigned char readValue = RB3;
-            INTCON0bits.T0IF = 0;
-        }
+    }
+}
+
+void interrupt ISR(void) {
+    static int oscillate = 0;
+    
+    if(INTCON0bits.T0IF == 1) {
+        RC4 = oscillate;
+        
+        oscillate = !oscillate;
+        
+        INTCON0bits.T0IF = 0;
     }
 }
