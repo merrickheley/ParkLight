80122e9e252f3c5bd61b60b76c4f43e2a4f62ac2
Jonathan Holland <jonathan.holland8@gmail.com>
2016-01-27 21:18:59 +1000

    Changing to read in pin value
    - Also removing the break on max value to ensure reading is actually working
    - Also reducing counts to 5, with a 10us delay this is equal to approx 16cm

--- HCSR04.c
+++ HCSR04.c
@@ -38,12 +38,13 @@ void HCSR04_Trigger(void) {
     PIN_US_TRIGGER = 1; //high
     __delay_us(15); //wait 15uS
     PIN_US_TRIGGER = 0; //low
+    char echo = PIN_US_ECHO;
     
     // Loop until we get a falling edge. 
     // TODO: Add some protection in here to break out if there's no falling edge?
     while (1) {
-        // If the pin is high
-        if (PIN_US_ECHO > 0) {
+
+        if (echo > 0) {
             // There has been a rising edge
             risingEdge = true;
             
@@ -52,18 +53,20 @@ void HCSR04_Trigger(void) {
             // If it is further away, we don't have to wait any longer.
             if (counter < UINT_FAST16_MAX) {
                counter++;
-            } else {
-                break;
-            }
+            }// else {
+            //    break;
+            //}
             
         // If a falling edge occurs, break out.
-        } else if (risingEdge == true && PIN_US_ECHO == 0) {
+        } else if (risingEdge == true && echo == 0) {
             break;
         }
+        // If the pin is high
+        char echo = PIN_US_ECHO;
         __delay_us(10);
     }
     
-    if (counter > 50) {
+    if (counter > 5) {
         PIN_LED_0 = LED_ON;
     }
 }
