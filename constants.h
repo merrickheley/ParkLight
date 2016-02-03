4f32f8b45a1dd4cc3a0f87f15d7a21401cc93ef0
Jonathan Holland <jonathan.holland8@gmail.com>
2016-02-03 19:43:53 +1000

    Adding defines for light values and changing pin arrangement

--- constants.h
+++ constants.h
@@ -16,17 +16,22 @@
 
 // HCSR04 Pins
 #define PIN_US_TRIGGER          RC2
-#define PIN_US_ECHO             RB5
+#define PIN_US_ECHO             RA2
 
 // TLC5926 Pins
-// TODO: Update these to be accurate
 #define PIN_LED_SDI             RC4
 #define PIN_LED_CLK             RC3
 #define PIN_LED_LE              RC6
 #define PIN_LED_OE              RC7
 
+// LED Array colour bitmap values
+#define RED_LIGHT 0x2000
+#define YELLOW_LIGHT 0x0800
+#define GREEN_LIGHT 0x0200
+#define OFF_LIGHT 0x0000
+
 // Button pins for EEPROM
-#define PIN_SET_YELLOW          RA4
-#define PIN_SET_RED             RC5
+#define BTN_SET_YELLOW          RB4
+#define BTN_SET_RED             RB5
 
 #endif	/* CONSTANTS_H */
