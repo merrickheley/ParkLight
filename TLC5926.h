6d4338214f23784f60ea840a73765081d07ae1ab
Jonathan Holland <jonathan.holland8@gmail.com>
2016-03-02 21:29:16 +1000

    Adding the display_LED function and the LedState struct
    - This display_LED functionality was previously used in main when testing transition of the LED's based on changing distances
    
    NOTE: If we are triggering HCSR04 less often than we were in testing (i.e. repeatedly in a while loop), then the transitionCounter will have a greater delay effect

--- TLC5926.h
+++ TLC5926.h
@@ -5,7 +5,16 @@
 
 #define TLC5926_US_DELAY   10
 
+typedef struct Led_State {
+    uint8_t ledState; // Red by default?
+    uint8_t transitionCounter;
+} LedState;
+
 void TLC5926_init(void);
 void TLC5926_SetLights(uint16_t bitmap);
+LedState display_LED(uint_fast16_t distanceCounter, uint8_t currentState, 
+        uint8_t transitionCounter);
+
+
 
 #endif	/* TLC5926_H */
