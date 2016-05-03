82a902b516368b288975fdf7e5dd0d15a8926ae8
Merrick Heley <merrick.heley@gmail.com>
2016-05-03 21:13:59 +1000

    Implement timeout mechanism. This is done by splitting up the transition mechanism from the state mechanism.

--- display.c
+++ display.c
@@ -13,44 +13,50 @@
 #include "constants.h"
 #include "TLC5926.h"
 
+#define LED_OFF_THRESH 25
+
 void display_LED(LedState *ledState, uint8_t reading, uint8_t yellow, uint8_t red) 
-{       
+{   
+    uint8_t oldLedState = ledState->state;
+    
     // If the state is green
-    if (ledState->state == STATE_GREEN) {
+    if (oldLedState == STATE_GREEN) {
         // If the counter is within the yellow threshold, transition
-        if (reading < yellow) {
+        if (reading < yellow)
             ledState->state = STATE_YELLOW;
-            TLC5926_SetLights(LIGHT_YELLOW);
-        }
     // If the state is yellow
-    } else if (ledState->state == STATE_YELLOW) {
+    } else if (oldLedState == STATE_YELLOW) {
         // If the state is within the red threshold, transition
-        if (reading < red) {
+        if (reading < red)
             ledState->state = STATE_RED;
-            TLC5926_SetLights(LIGHT_RED);
-        }
         // If the state is outside the green threshold, transition
-        else if (reading > (yellow + LIGHT_THRESH_OFFSET)) {
+        else if (reading > (yellow + LIGHT_THRESH_OFFSET))
             ledState->state = STATE_GREEN;
-            TLC5926_SetLights(LIGHT_GREEN);                
-        }
     // If the state is red
-    } else if (ledState->state == STATE_RED) {
+    } else if (oldLedState == STATE_RED) {
         // If the state is within the yellow threshold, transition
-        if (reading > (red + LIGHT_THRESH_OFFSET)) {
+        if (reading > (red + LIGHT_THRESH_OFFSET))
             ledState->state = STATE_YELLOW;
-            TLC5926_SetLights(LIGHT_YELLOW);
-            ledState->turnoffCounter = 0;
-        } 
-        // If there is no change in state
-        else {
-            // Increment the state counter
+    }
+    
+    // If the led state hasn't been changed
+    if (ledState->state == oldLedState) {
+        // Turn the light off if we're over the threshold
+        if (ledState->turnoffCounter >= LED_OFF_THRESH)
+            TLC5926_SetLights(LIGHT_OFF);
+        // Otherwise increment the counter
+        else
             ledState->turnoffCounter++;
-            // After approx five seconds of red
-            if (ledState->turnoffCounter > 5) { 
-                TLC5926_SetLights(LIGHT_OFF);
-            }
-        }
+    } 
+    // If the state has changed, updates the light and reset the counter
+    else {
+        ledState->turnoffCounter = 0;
+        if (ledState->state == STATE_RED)
+            TLC5926_SetLights(LIGHT_RED);
+        else if (ledState->state == STATE_YELLOW)
+            TLC5926_SetLights(LIGHT_YELLOW);
+        else if (ledState->state == STATE_GREEN)
+            TLC5926_SetLights(LIGHT_GREEN);
     }
 }
 
