18cbd233d3e1f67ae166b346e0c4b6f4d277c245
Jonathan Holland <jonathan.holland8@gmail.com>
2016-08-24 19:04:59 +1000

    UART function changed to be fixed to 19200 (to get a higher uart rate for the standard 4Mhz clock)

--- uart.c
+++ uart.c
@@ -17,26 +17,19 @@
 char UART_init(const long int baudrate, const long int clock, bool transmit, bool receive)
 {
     unsigned int x;
-    x = (clock - baudrate*64)/(baudrate*64);   //SPBRG for Low Baud Rate
-    if(x>255)                                       //If High Baud Rage Required
-    {
-        x = (clock - baudrate*16)/(baudrate*16); //SPBRG for High Baud Rate
-        BRGH = 1;                                     //Setting High Baud Rate
-    }
-    if(x<256)
-    {
-        SPBRG = x;                                    // Writing SPBRG Register
-        SYNC = 0;                                     // Setting Asynchronous Mode, ie UART
-        SPEN = 1;                                     // Enables Serial Port    
-        if (transmit == true)
-            TXEN = 1;                                 // Enables Transmission
+    SPBRG = (clock - baudrate*16)/(baudrate*16); //SPBRG for High Baud Rate
+    BRGH = 1;
+
+    SYNC = 0;                                     // Setting Asynchronous Mode, ie UART
+    SPEN = 1;                                     // Enables Serial Port    
+    if (transmit == true)
+        TXEN = 1;                                 // Enables Transmission
         
-        if (receive == true)
-            CREN = 1;                                 // Enables Continuous Reception
+    if (receive == true)
+        CREN = 1;                                 // Enables Continuous Reception
         
-        return 1;                                     // Returns 1 to indicate Successful Completion
-    }
-    return 0;                                       // Returns 0 to indicate UART initialization failed
+    return 1;                                     // Returns 1 to indicate Successful Completion
+                                // Returns 0 to indicate UART initialization failed
 }
 
 void UART_write(char data)
